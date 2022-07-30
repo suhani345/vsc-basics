ef333f9e58eb0886bb999e1152fb2ea71db8a1ec
suhani345 <92861531+suhani345@users.noreply.github.com>
2022-07-30 17:38:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hi I am console screen");
+console.log("hi i am suhani");
+var fruit="apple";
+console.log(fruit);
+var first_name="suhani ";
+var last_name="singhal";
+console.log (last_name);
+console.log(first_name+last_name);
+console.log(first_name.length);
+var new_name=first_name.replace("suhani","harshita")
+console.log (new_name);
+var capital=first_name.toUpperCase();
+console.log(capital);
+var square_root=Math.sqrt(26);
+console.log (square_root);
+var minimum =Math.min(15,10,12,14,13);
+console.log(minimum);
+var power=Math.pow(4,2);
+console.log(power);     
